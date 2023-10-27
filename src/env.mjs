8a3e696b54d81f8f3933db434deecc464d339596
ashleendaly <Ashleen.Daly@res-group.com>
2023-10-28 00:33:15 +0100

    removed not needed

--- src/env.mjs
+++ src/env.mjs
@@ -31,7 +31,6 @@ export const env = createEnv({
     // Add ` on ID and SECRET if you want to make sure they're not empty
     DISCORD_CLIENT_ID: z.string(),
     DISCORD_CLIENT_SECRET: z.string(),
-    GOOGLE_GEOLOCATION_API_KEY: z.string(),
   },
 
   /**
@@ -54,7 +53,6 @@ export const env = createEnv({
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
     DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
-    GOOGLE_GEOLOCATION_API_KEY: process.env.GOOGLE_GEOLOCATION_API_KEY
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
