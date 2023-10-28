cef8ead34acb17c1f05b31199a486bd94f091172
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2023-10-28 10:08:56 +0100

    added customer data to population script

--- prisma/populate.ts
+++ prisma/populate.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
-import { ProductModel } from "@prisma/schemas";
-import { z } from "zod";
+import { ProductModel, CustomerModel, AddressModel } from "@prisma/schemas";
+import { custom, z } from "zod";
 
 const db = new PrismaClient();
 
@@ -15,6 +15,8 @@ async function add_products() {
     await fetch(GG_URL + "products/").then((e) => e.json()),
   );
 
+  console.log(`found ${data.length} products to add`);
+
   for (let i = 0; i < data.length / CHUNK_SIZE; i += 1) {
     const chunk = data.slice(i * CHUNK_SIZE, i * CHUNK_SIZE + CHUNK_SIZE);
     try {
@@ -28,8 +30,43 @@ async function add_products() {
   console.log("finished adding products");
 }
 
+async function add_customers() {
+  console.log("adding customers");
+  const customerSchema = z.array(
+    CustomerModel.omit({ addressId: true })
+      .extend({
+        address: AddressModel.omit({ id: true }),
+      })
+      .transform(({ address, ...rest }) => ({
+        ...rest,
+        address: { create: address },
+      })),
+  );
+
+  const data = customerSchema.parse(
+    await fetch(GG_URL + "customers/").then((e) => e.json()),
+  );
+
+  data[0]!.address;
+
+  console.log(`found ${data.length} customers to add`);
+  let i = 1;
+  for (const customer of data) {
+    try {
+      await db.customer.create({ data: customer });
+      console.log(`added chunk ${i}`);
+    } catch {
+      console.log(`failed chunk ${i}`);
+    }
+    i += 1;
+  }
+
+  console.log("finished adding customers");
+}
+
 async function main() {
-  await add_products();
+  // await add_products();
+  await add_customers();
 }
 
 main()
