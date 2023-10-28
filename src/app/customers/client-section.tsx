b0f6bbcba515d8aeb2892478610c9cdae77ed5c3
Petros <petroskitazos@gmail.com>
2023-10-28 16:00:26 +0100

    fixed import

--- src/app/customers/client-section.tsx
+++ src/app/customers/client-section.tsx
@@ -1,8 +1,10 @@
 "use client";
 import { type Customer } from "@prisma/client";
-import DataTable from "~/components/ui/data-table/data-table";
+import { DataTable } from "~/components/ui/data-table/data-table";
 import { columns } from "./columns";
 
 export function ClientSection({ customerData }: { customerData: Customer[] }) {
-  return <DataTable columns={columns} data={customerData} />;
+  return (
+    <DataTable columns={columns} data={customerData} filterableFields={[]} />
+  );
 }
