03035d64ed6bcffe9d34e0576ffd0e9b49cfd359
ashleendaly <Ashleen.Daly@res-group.com>
2023-10-28 21:21:09 +0100

    added stacked bar chart

--- src/app/analytics/revenue/page.tsx
+++ src/app/analytics/revenue/page.tsx
@@ -5,47 +5,69 @@ import { SpinnerComponent } from "~/components/ui/spinner";
 import { api } from "~/trpc/react";
 
 interface OrderWithPrice {
-    Id: number;
-    DateCreated: Date;
-    OrderTotal: number;
-    OrderStatus: string;
-    addressId: string;
-    CustomerId: number;
-    Products: Product[];
-  }
-
-  interface Product {
-    Category: string
-  }
+  Id: number;
+  DateCreated: Date;
+  OrderTotal: number;
+  OrderStatus: string;
+  addressId: string;
+  CustomerId: number;
+}
 
 const Page = () => {
-  const { data: orders, isLoading } =
-    api.analytics.getOrdersAndPrice.useQuery();
+  const { data: allOrders } = api.analytics.getAllOrders.useQuery();
 
-    if (!orders) return
+  const { data: revenueByCategory } =
+    api.analytics.getGroupedOrdersAndCategories.useQuery();
 
-    orders.map((order: OrderWithPrice ) => {
-        console.log(order)
-      })
+  if (!allOrders || !revenueByCategory) return;
 
-      const cumulativeSum = (sum => (value: number) => sum += value)(0);
+  const cumulativeSum = (
+    (sum) => (value: number) =>
+      (sum += value)
+  )(0);
 
+  // ["2019", "2020", "2021", "2022", "2023"]
+
+  console.log(revenueByCategory)
+
+  let test = Object.keys(revenueByCategory).map((category) => {
+    return {
+      x: ["2019", "2020", "2021", "2022", "2023"], 
+      y: revenueByCategory[category],
+      name: `${category}`,
+      type: "bar"
+    }
+  })
 
   return (
-    <div className="h-[88dvh]">
-        <div className="flex h-full justify-center">
+    <div className="h-[88dvh] ">
+      <div className="flex h-full justify-center">
         <Plot
-        data={[
-          {
-            x: orders.map((order) => order.DateCreated),
-            y: orders.map((order) => order.OrderTotal).map(cumulativeSum),
-            type: 'scatter',
-            mode: 'lines+markers',
-            marker: {color: 'red'},
-          },
-        ]}
-        layout={ {width: 700, height: 700, title: 'A Fancy Plot'} }
-      />
+          data={[
+            {
+              x: allOrders.map((order) => order.DateCreated),
+              y: allOrders.map((order) => order.OrderTotal).map(cumulativeSum),
+              type: "scatter",
+              mode: "lines",
+              marker: { color: "red" },
+            },
+          ]}
+          layout={{
+            width: 900,
+            height: 700,
+            title: "Total Revenue",
+          }}
+        />
+        <Plot
+        // @ts-ignore
+          data={test}
+          layout={{
+            width: 900,
+            height: 700,
+            title: "Total Revenue Per Category",
+            barmode: "stack",
+          }}
+        />
       </div>
     </div>
   );
