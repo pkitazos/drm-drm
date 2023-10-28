8b467903c50c865528fc0cc660f1c80821d32e60
Ashleen Daly <54708608+ashleendaly@users.noreply.github.com>
2023-10-28 23:56:34 +0100

    Yoy chart (#29)
    * revenue graph and sidebar
    
    * lock merge
    
    * fix lock
    
    * added stacked bar chart
    
    * added jakes bs
    
    * change map height
    
    * yoy bs
    
    * fix eslint
    
    * death
    
    * got the chart bruh

--- src/app/analytics/revenue/page.tsx
+++ src/app/analytics/revenue/page.tsx
@@ -2,16 +2,11 @@ import { api } from "~/trpc/server";
 import ClientPlot from "./client-plot";
 
 const Page = async () => {
-  const allOrders = await api.analytics.getAllOrders.query();
+  const yoy = await api.analytics.getYoyRevenue.query();
   const revenueByCategory =
     await api.analytics.getGroupedOrdersAndCategories.query();
 
-  if (!allOrders || !revenueByCategory) return;
-
-  const cumulativeSum = (
-    (sum) => (value: number) =>
-      (sum += value)
-  )(0);
+  if (!yoy || !revenueByCategory) return;
 
   const categoryData = Object.keys(revenueByCategory).map((category) => {
     return {
@@ -22,24 +17,43 @@ const Page = async () => {
     };
   });
 
+  const dates = [
+    new Date("2020-01-01"),
+    new Date("2020-04-01"),
+    new Date("2020-07-01"),
+    new Date("2020-10-01"),
+    new Date("2021-01-01"),
+    new Date("2021-04-01"),
+    new Date("2021-07-01"),
+    new Date("2021-10-01"),
+    new Date("2022-01-01"),
+    new Date("2022-04-01"),
+    new Date("2022-07-01"),
+    new Date("2022-10-01"),
+    new Date("2023-01-01"),
+    new Date("2023-04-01"),
+    new Date("2023-07-01"),
+    new Date("2023-10-01"),
+  ];
+
   return (
     <div className="h-[88dvh] ">
       <div className="flex h-full justify-center">
         <ClientPlot
           data={[
             {
-              x: allOrders.map((order) => order.DateCreated),
-              y: allOrders.map((order) => order.OrderTotal).map(cumulativeSum),
+              x: dates,
+              y: yoy,
               type: "scatter",
               mode: "lines",
               marker: { color: "red" },
             },
           ]}
-          title="Total Revenue"
+          title="Year-Over-Year Quarterly Revenue (2019-2023)"
         />
         <ClientPlot
           data={categoryData}
-          title="Total Revenue Per Category"
+          title="Total Revenue Per Category (2019-2023)"
           barmode="stack"
         />
       </div>
--- src/server/api/routers/analytics.ts
+++ src/server/api/routers/analytics.ts
@@ -16,14 +16,6 @@ export const analyticsRouter = createTRPCRouter({
     });
   }),
 
-  getAllOrders: publicProcedure.query(async ({ ctx }) => {
-    return await ctx.db.order.findMany({
-      orderBy: {
-        DateCreated: "asc",
-      },
-    });
-  }),
-
   getGroupedOrdersAndCategories: publicProcedure.query(async ({ ctx }) => {
     type CategoryDict = Record<string, number[]>;
 
@@ -61,5 +53,90 @@ export const analyticsRouter = createTRPCRouter({
     };
 
     return await getDataForYears();
+  }),
+
+  getYoyRevenue: publicProcedure.query(async ({ ctx }) => {
+    const orders = await ctx.db.order.findMany({
+      orderBy: {
+        DateCreated: "asc",
+      },
+    });
+
+    const yoyArr = new Array<number>(20).fill(0);
+
+    const yoy = (order: { DateCreated: Date; OrderTotal: number }) => {
+      switch (true) {
+        case order.DateCreated < new Date("2019-03-31"):
+          yoyArr[0] += order.OrderTotal;
+          break;
+        case order.DateCreated < new Date("2019-06-30"):
+          yoyArr[1] += order.OrderTotal;
+          break;
+        case order.DateCreated < new Date("2019-09-30"):
+          yoyArr[2] += order.OrderTotal;
+          break;
+        case order.DateCreated < new Date("2019-12-31"):
+          yoyArr[3] += order.OrderTotal;
+          break;
+        case order.DateCreated < new Date("2020-03-31"):
+          yoyArr[4] += order.OrderTotal;
+          break;
+        case order.DateCreated < new Date("2020-06-30"):
+          yoyArr[5] += order.OrderTotal;
+          break;
+        case order.DateCreated < new Date("2020-09-30"):
+          yoyArr[6] += order.OrderTotal;
+          break;
+        case order.DateCreated < new Date("2020-12-31"):
+          yoyArr[7] += order.OrderTotal;
+          break;
+        case order.DateCreated < new Date("2021-03-31"):
+          yoyArr[8] += order.OrderTotal;
+          break;
+        case order.DateCreated < new Date("2021-06-30"):
+          yoyArr[9] += order.OrderTotal;
+          break;
+        case order.DateCreated < new Date("2021-09-30"):
+          yoyArr[10] += order.OrderTotal;
+          break;
+        case order.DateCreated < new Date("2021-12-31"):
+          yoyArr[11] += order.OrderTotal;
+          break;
+        case order.DateCreated < new Date("2022-03-31"):
+          yoyArr[12] += order.OrderTotal;
+          break;
+        case order.DateCreated < new Date("2022-06-30"):
+          yoyArr[13] += order.OrderTotal;
+          break;
+        case order.DateCreated < new Date("2022-09-30"):
+          yoyArr[14] += order.OrderTotal;
+          break;
+        case order.DateCreated < new Date("2022-12-31"):
+          yoyArr[15] += order.OrderTotal;
+          break;
+        case order.DateCreated < new Date("2023-03-31"):
+          yoyArr[16] += order.OrderTotal;
+          break;
+        case order.DateCreated < new Date("2023-06-30"):
+          yoyArr[17] += order.OrderTotal;
+          break;
+        case order.DateCreated < new Date("2023-09-30"):
+          yoyArr[18] += order.OrderTotal;
+          break;
+        case order.DateCreated < new Date("2023-12-31"):
+          yoyArr[19] += order.OrderTotal;
+          break;
+      }
+    };
+
+    orders.map((order) => yoy(order));
+
+    const results = new Array<number>(16).fill(0);
+
+    for (let i=0; i< yoyArr.length-4; i++) {
+      results[i] = (yoyArr[i+4]!/yoyArr[i]!)-1
+    }
+
+    return results;
   }),
 });
