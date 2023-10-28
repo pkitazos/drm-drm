ec53a99aa5a3c4071271023689f309355675e766
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2023-10-28 21:52:25 +0100

    finished customer/id page

--- src/app/customers/[id]/page.tsx
+++ src/app/customers/[id]/page.tsx
@@ -3,6 +3,7 @@ import { ClientSection } from "./client-section";
 import Image from "next/image";
 import { Separator } from "~/components/ui/separator";
 import { Loyalty } from "~/components/loyalty";
+import { AddressDisplay } from "~/components/address-display";
 
 export default async function Page({
   params: { id },
@@ -14,26 +15,35 @@ export default async function Page({
     CustomerId: parseInt(id),
   });
 
+  const address = await api.addresses.getById.query({ id: userData.addressId });
+
   return (
     <main className="grid h-[88dvh] place-items-center">
-      <section className="flex w-full justify-between">
+      <section className="flex w-full justify-center gap-16">
         <div className="flex flex-col justify-center">
           <Image
             src={userData.avatar}
             width={300}
             height={300}
             alt="user Avatar"
-            className="rounded-full border"
+            className="rounded-full border shadow-md"
           />
-          <h1 className="text-center">
+          <h1 className="mt-2 text-center text-2xl font-bold">
             {userData.first_name} {userData.last_name}
           </h1>
           <Loyalty level={userData.LoyaltyLevel} />
         </div>
-        <div></div>
-        <div>
-          Email: <p>{userData.email}</p>
-          Phone: <p>{userData.phone_number}</p>
+        <div className="mt-8 flex flex-col gap-6">
+          <div className="rounded-md bg-accent p-5 shadow-md">
+            <p>
+              <span className="font-semibold">Email:</span> {userData.email}
+            </p>
+            <p>
+              <span className="font-semibold">Phone:</span>{" "}
+              {userData.phone_number}
+            </p>
+          </div>
+          <AddressDisplay address={address} />
         </div>
       </section>
       <Separator className="my-5 mt-10" />
--- src/server/api/routers/address.ts
+++ src/server/api/routers/address.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
-export const customerRouter = createTRPCRouter({
+export const addressRouter = createTRPCRouter({
   getById: publicProcedure
     .input(z.object({ id: z.string() }))
     .query(async ({ ctx, input: { id: Id } }) => {
