6404e1b46946ec4ef40b01a6c95dcea8844339e0
Petros <petroskitazos@gmail.com>
2023-10-28 17:29:06 +0100

    removed unused component

--- src/app/products/new/page.tsx
+++ src/app/products/new/page.tsx
@@ -4,26 +4,6 @@ export default function Page() {
   return (
     <main className="flex h-[80dvh] justify-center gap-5 p-5">
       <CreateProductForm />
-
-      {/* <div className="relative flex basis-1/3 flex-col items-center justify-center rounded border">
-        <Image
-          className="flex h-full object-contain"
-          src="/test-guitar.jpg"
-          height={600}
-          width={280}
-          alt="test guitar"
-        />
-        <Button
-          variant="ghost"
-          className="absolute bottom-0 right-0 h-max"
-          size="icon"
-        >
-          <FileUploadButton />
-        </Button>
-      </div>
-      <div className="basis-2/3 bg-green-100">
-        <ProductForm />
-      </div> */}
     </main>
   );
 }
