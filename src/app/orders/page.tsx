80306b11aa608b8a26b4f72c09f806fc6e5badf3
Rory McDermid <2566302M@student.gla.ac.uk>
2023-10-28 15:08:21 +0100

    orders column and supporting files finished

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,3 +1,11 @@
-export default function Page() {
-  return <main className="grid h-[88dvh] place-items-center">orders</main>;
+import { api } from "~/trpc/server";
+import { ClientSection } from "./client-section";
+
+export default async function Page() {
+  const data = await api.orders.getAll.query({ page: 1 });
+  return (
+    <main className="flex h-[88dvh] justify-center px-16 pt-6">
+      <ClientSection orderData={data} />
+    </main>
+  );
 }
