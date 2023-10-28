a63d0c3a7cd73a992623dfd447e9abf5e801eb39
Petros <93052465+pkitazos@users.noreply.github.com>
2023-10-28 12:28:16 +0100

    New product (#7)
    * added upload thing
    
    * added some styling to product pages
    
    * styled upload button
    
    * updated component name in products/[id] page
    
    ---------
    
    Co-authored-by: ashleendaly <Ashleen.Daly@res-group.com>

--- src/app/products/new/page.tsx
+++ src/app/products/new/page.tsx
@@ -1,5 +1,27 @@
+import Image from "next/image";
+import { FileUploadButton } from "~/components/file-upload-button";
+import { Button } from "~/components/ui/button";
+
 export default function Page() {
   return (
-    <main className="grid h-[90dvh] place-items-center">products/new</main>
+    <main className="flex h-[80dvh] justify-center gap-5 p-5">
+      <div className="relative flex basis-1/3 flex-col items-center justify-center rounded border">
+        <Image
+          className="flex h-full object-contain"
+          src="/test-guitar.jpg"
+          height={600}
+          width={280}
+          alt="test guitar"
+        />
+        <Button
+          variant="ghost"
+          className="absolute bottom-0 right-0 h-max"
+          size="icon"
+        >
+          <FileUploadButton />
+        </Button>
+      </div>
+      <div className="basis-2/3 bg-green-100">Test form</div>
+    </main>
   );
 }
---
+++ src/components/file-upload-button.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { PenIcon } from "lucide-react";
+import { UploadButton } from "~/lib/uploadthing";
+import { cn } from "~/lib/utils";
+
+export function FileUploadButton() {
+  return (
+    <UploadButton
+      endpoint="imageUploader"
+      onClientUploadComplete={(res) => {
+        console.log("Files: ", res);
+        alert("Upload Completed");
+      }}
+      onUploadError={(error: Error) => {
+        alert(`ERROR! ${error.message}`);
+      }}
+      content={{
+        button({ isUploading }) {
+          return (
+            <PenIcon
+              className={cn(
+                "h-4 w-4 cursor-pointer stroke-slate-500",
+                isUploading && "stroke-slate-400",
+              )}
+            />
+          );
+        },
+        allowedContent() {
+          return <></>;
+        },
+      }}
+    />
+  );
+}
