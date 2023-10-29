82b67a0d2f7ed408d023b755a563459705779a52
ashleendaly <Ashleen.Daly@res-group.com>
2023-10-29 03:03:46 +0000

    fix

--- src/components/signin-button.tsx
+++ src/components/signin-button.tsx
@@ -1,10 +1,11 @@
-"use client";
-import { Music, ShoppingCart } from "lucide-react";
-import { useCart } from "~/lib/cart-context";
-import { Breadcrumbs } from "./breadcrumbs";
+import React from "react";
+import { getServerAuthSession } from "~/server/auth";
+import { api } from "~/trpc/server";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Button } from "./ui/button";
+import { signIn, signOut } from "next-auth/react";
 import { CartCard } from "./cart-card";
 import { Checkout } from "./checkout";
-import { Button } from "./ui/button";
 import {
   Sheet,
   SheetContent,
@@ -15,20 +16,39 @@ import {
 } from "./ui/sheet";
 import { cn } from "~/lib/utils";
 
-export default function Header() {
-  return (
-    <header className="fixed left-14 top-0 z-50 flex h-[14dvh] w-[calc(100%-3.5rem)] justify-between bg-background px-7 pt-3">
-      <div className="flex flex-col items-start justify-center gap-2">
-        <Breadcrumbs />
-        <div className="flex items-center gap-3">
-          <Music className="stroke-[3] text-orange-500" />
-          <h1 className="text-2xl font-semibold">drum-drum</h1>
-        </div>
+export const AuthButton = async () => {
+  const session = await getServerAuthSession();
+  if (session) {
+    const userId = session?.user.id;
+    const data = await api.users.getIcon.query({ id: userId ?? "" });
+    return (
+      <div className="flex gap-2">
+        <Avatar>
+          <AvatarImage src={data?.UserLinking[0]?.customer.avatar} />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+        <Button
+          variant="outline"
+          size="icon"
+          className="h-10 w-20"
+          onClick={() => signOut()}
+        >
+          Sign Out
+        </Button>
       </div>
-      <Cart />
-    </header>
+    );
+  }
+  return (
+    <Button
+      variant="outline"
+      size="icon"
+      className="h-10 w-20"
+      onClick={() => signIn()}
+    >
+      Sign in
+    </Button>
   );
-}
+};
 
 function Cart() {
   const { contents } = useCart();
