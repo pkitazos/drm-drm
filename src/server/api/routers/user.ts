22d705e9505b78f550d81c38e911efaf1caead95
Ashleen Daly <54708608+ashleendaly@users.noreply.github.com>
2023-10-29 04:08:10 +0000

    Loyality data (#43)
    * add data
    
    * plum plum

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -23,4 +23,36 @@ export const userRouter = createTRPCRouter({
         }
       });
     }),
+    getLoyalty: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ ctx, input: { id: Id } }) => {
+      return await ctx.db.user.findFirstOrThrow({
+        where: {
+          id: Id,
+        },
+        include: {
+            UserLinking: {
+                select: {
+                    customer: {
+                        select: {
+                            LoyaltyLevel: true
+                        }
+                    }
+                }
+            }
+        }
+      });
+    }),
+    getRole: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ ctx, input: { id: Id } }) => {
+      return await ctx.db.user.findFirstOrThrow({
+        where: {
+          id: Id,
+        },
+        select: {
+          role: true
+        }
+      });
+    }),
 });
