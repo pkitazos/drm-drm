27c7a51498c892487890be48214bf44f90801414
ashleendaly <Ashleen.Daly@res-group.com>
2023-10-29 01:41:08 +0100

    fix buttons

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -5,33 +5,28 @@ import { Label } from "~/components/ui/label";
 const page = () => {
   return (
     <ol className="flex h-[70dvh] items-center justify-center gap-10">
-      <li className="relative flex h-full basis-1/4 items-center justify-center rounded border bg-gray-100 transition-transform duration-200 hover:scale-[1.02] hover:bg-orange-100">
-        <Link
-          className="grid justify-items-center gap-2"
-          href={"analytics/loyalties"}
-        >
-          <BarChart />
-          <Label>Loyalty Demographics</Label>
-        </Link>
-      </li>
-      <li className="relative flex h-full basis-1/4 items-center justify-center rounded border bg-gray-100 transition-transform duration-200 hover:scale-[1.02] hover:bg-orange-100">
-        <Link
-          className="grid justify-items-center gap-2"
-          href={"analytics/map"}
-        >
-          <Map />
-          <Label>Orders</Label>
-        </Link>
-      </li>
-      <li className="relative flex h-full basis-1/4 items-center justify-center rounded border  bg-gray-100 transition-transform duration-200 hover:scale-[1.02] hover:bg-orange-100">
-        <Link
-          className="grid justify-items-center gap-2"
-          href={"analytics/revenue"}
-        >
-          <LineChart />
-          <Label>Revenue</Label>
-        </Link>
-      </li>
+      <Link
+        className="relative flex h-full basis-1/4 flex-col items-center justify-center gap-2 rounded  border bg-gray-100 transition-transform duration-200 hover:scale-[1.02] hover:bg-orange-100"
+        href={"analytics/loyalties"}
+      >
+        <BarChart />
+        <Label>Loyalty Demographics</Label>
+      </Link>
+
+      <Link
+        className="relative flex h-full basis-1/4 flex-col items-center justify-center gap-2 rounded  border bg-gray-100 transition-transform duration-200 hover:scale-[1.02] hover:bg-orange-100"
+        href={"analytics/map"}
+      >
+        <Map />
+        <Label>Orders</Label>
+      </Link>
+      <Link
+        className="relative flex h-full basis-1/4 flex-col items-center justify-center gap-2 rounded  border bg-gray-100 transition-transform duration-200 hover:scale-[1.02] hover:bg-orange-100"
+        href={"analytics/revenue"}
+      >
+        <LineChart />
+        <Label>Revenue</Label>
+      </Link>
     </ol>
   );
 };
