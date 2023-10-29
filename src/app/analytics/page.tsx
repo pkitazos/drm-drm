ca4017737b7c9196b25c67a7794c280e59383a06
ashleendaly <Ashleen.Daly@res-group.com>
2023-10-29 01:19:58 +0100

    analytics

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -1,7 +1,44 @@
+import Link from "next/link";
+import Image from "next/image";
+
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <ol className="flex h-[70dvh] items-center justify-center gap-10">
+      <li className="flex h-full basis-1/4 items-center justify-center rounded border transition-transform duration-150 hover:scale-[1.02]">
+        <Link href={"analytics/loyalties"}>
+          <Image
+            className="flex object-contain"
+            width={450}
+            src="/Loyalty.JPG"
+            height={900}
+            alt={"Revenue"}
+          />
+        </Link>
+      </li>
+      <li className="flex h-full basis-1/4 items-center justify-center rounded border transition-transform duration-150 hover:scale-[1.02]">
+        <Link href={"analytics/map"}>
+          <Image
+            className="flex object-contain"
+            width={450}
+            src="/Map.JPG"
+            height={900}
+            alt={"Revenue"}
+          />
+        </Link>
+      </li>
+      <li className="flex h-full basis-1/4 items-center justify-center rounded border transition-transform duration-150 hover:scale-[1.02]">
+        <Link href={"analytics/revenue"}>
+          <Image
+            className="flex object-contain"
+            width={450}
+            src="/Revenue.JPG"
+            height={900}
+            alt={"Revenue"}
+          />
+        </Link>
+      </li>
+    </ol>
+  );
+};
 
-export default page
+export default page;
