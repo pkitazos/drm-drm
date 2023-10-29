d1fdf32fd68097954b30ce8fe5186886657b6e16
Petros <93052465+pkitazos@users.noreply.github.com>
2023-10-29 01:37:52 +0000

    started checkout implementation (#33)
    * created cart context and shopping cart sheet
    
    * added shop/[item] page and ability to add/remove from cart

---
+++ src/lib/cart-context.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { type Product } from "@prisma/client";
+import { type ReactNode, createContext, useContext, useState } from "react";
+
+interface cardContextType {
+  contents: Product[];
+  add: (product: Product) => void;
+  remove: (idx: number) => void;
+}
+
+const cartContext = createContext<cardContextType>({
+  contents: [],
+  add: () => {
+    return;
+  },
+  remove: () => {
+    return;
+  },
+});
+
+export const CartContextProvider = ({ children }: { children: ReactNode }) => {
+  const [selectedProducts, setSelectedProducts] = useState<Product[]>([]);
+
+  const add = (product: Product) => {
+    setSelectedProducts((prev) => [...prev, product]);
+  };
+
+  const remove = (idx: number) => {
+    setSelectedProducts((prev) => prev.filter((_, i) => i !== idx));
+  };
+
+  return (
+    <cartContext.Provider value={{ add, remove, contents: selectedProducts }}>
+      {children}
+    </cartContext.Provider>
+  );
+};
+
+export const useCart = () => {
+  return useContext(cartContext);
+};
