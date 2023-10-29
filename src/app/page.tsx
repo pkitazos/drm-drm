82a4e1ea60bd8be171ea0c5402a596ea479f28ef
Rory McDermid <2566302M@student.gla.ac.uk>
2023-10-29 04:29:27 +0000

    front page made better

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <main className="grid h-[70dvh] place-items-center">plum plum</main>;
+  return (
+    <main className="grid h-[70dvh] place-items-center text-3xl">
+      <p>
+        Welcome to the{" "}
+        <span className="font-semibold text-orange-500">GuitarGuitar</span>{" "}
+        website!
+      </p>
+      <p>Navigate to pages with the taskbar on the left</p>
+    </main>
+  );
 }
