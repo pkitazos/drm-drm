a297ca54d549e4d7ad0f6443c96baa85efcdd25d
RoryMcDermid <97927154+RoryMcDermid@users.noreply.github.com>
2023-10-29 02:15:04 +0000

    got order-status-display working (#34)
    * got order-status-display working
    
    * fixed smells in order-status-display
    
    ---------
    
    Co-authored-by: Rory McDermid <2566302M@student.gla.ac.uk>
    Co-authored-by: Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>

---
+++ src/app/orders/[id]/order-status-display.tsx
@@ -0,0 +1,104 @@
+import { type OrderStatus } from "@prisma/client";
+import {
+  MailCheck,
+  Package,
+  PackageCheck,
+  PackageOpen,
+  PackageX,
+  Truck,
+} from "lucide-react";
+import { cn } from "~/lib/utils";
+
+const dict = {
+  Cancelled: 0,
+  Placed: 1,
+  Dispatched: 2,
+  Delivering: 3,
+  Delivered: 4,
+  Completed: 5,
+};
+
+export const OrderStatusDisplay = ({ status }: { status: OrderStatus }) => {
+  const ord = dict[status]!;
+
+  return (
+    <ol className="relative flex gap-12 border-t border-gray-200 text-gray-500 dark:border-gray-700 dark:text-gray-400">
+      <li>
+        <span
+          className={cn(
+            `absolute -top-10 flex h-8 w-8 items-center justify-center rounded-full ring-white`,
+            ord > 0
+              ? "bg-green-200 text-green-500"
+              : "bg-gray-100 text-gray-700",
+          )}
+        >
+          <MailCheck />
+        </span>
+        <h3 className="font-medium leading-tight">Placed</h3>
+      </li>
+      <li>
+        <span
+          className={cn(
+            `absolute -top-10 flex h-8 w-8 items-center justify-center rounded-full ring-white`,
+            ord > 1
+              ? "bg-green-200 text-green-500"
+              : "bg-gray-100 text-gray-700",
+          )}
+        >
+          <Package />
+        </span>
+        <h3 className="font-medium leading-tight">Dispatched</h3>
+      </li>
+      <li>
+        <span
+          className={cn(
+            `absolute -top-10 flex h-8 w-8 items-center justify-center rounded-full ring-white`,
+            ord > 2
+              ? "bg-green-200 text-green-500"
+              : "bg-gray-100 text-gray-700",
+          )}
+        >
+          <Truck />
+        </span>
+        <h3 className="font-medium leading-tight">Delivering</h3>
+      </li>
+      <li>
+        <span
+          className={cn(
+            `absolute -top-10 flex h-8 w-8 items-center justify-center rounded-full ring-white`,
+            ord > 3
+              ? "bg-green-200 text-green-500"
+              : "bg-gray-100 text-gray-700",
+          )}
+        >
+          <PackageCheck />
+        </span>
+        <h3 className="font-medium leading-tight">Delivered</h3>
+      </li>
+      <li>
+        <span
+          className={cn(
+            `absolute -top-10 flex h-8 w-8 items-center justify-center rounded-full ring-white`,
+            ord > 4
+              ? "bg-green-200 text-green-500"
+              : "bg-gray-100 text-gray-700",
+          )}
+        >
+          <PackageOpen />
+        </span>
+        <h3 className="font-medium leading-tight">Completed</h3>
+      </li>
+      <li>
+        <span
+          className={cn(
+            `absolute -top-10 flex h-8 w-8 items-center justify-center rounded-full ring-white`,
+            ord === 0 ? "bg-red-200 text-red-500" : "bg-gray-100 text-gray-700",
+          )}
+        >
+          <PackageX />
+        </span>
+        <h3 className="font-medium leading-tight">Cancelled</h3>
+      </li>
+    </ol>
+  );
+};
--- src/app/orders/[id]/page.tsx
+++ src/app/orders/[id]/page.tsx
@@ -5,6 +5,7 @@ import { ItemCard } from "~/components/item-card";
 import { Separator } from "~/components/ui/separator";
 import { currencyFormatter } from "~/lib/currency";
 import { api } from "~/trpc/server";
+import { OrderStatusDisplay } from "./order-status-display";
 
 export default async function Page({
   params: { id },
@@ -12,19 +13,20 @@ export default async function Page({
   params: { id: string };
 }) {
   const order = await api.orders.getById.query({ id: parseInt(id) });
-
   const address = await api.addresses.getById.query({ id: order.addressId });
-
   const customer = await api.customers.getById.query({
     id: order.CustomerId,
   });
 
   return (
-    <main className="flex flex-col">
-      {/* <OrderStatusDisplay/> */}
-      <section className="flex flex-row justify-around">
+    <main className="flex flex-col items-center justify-center">
+      <div className="mb-8 mt-3 w-3/5">
+        <OrderStatusDisplay status={order.OrderStatus} />
+      </div>
+
+      <section className="flex flex-row justify-around gap-10">
         <div className="flex flex-col gap-8">
-          <div className="rounded-md bg-accent p-5 shadow-md">
+          <div className="mt-8 rounded-md bg-accent p-5 shadow-md">
             <h1>
               <span className="font-semibold">Order Placed On:</span>{" "}
               {format(order.DateCreated, "PPP")}
